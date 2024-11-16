bc13ed50f4f9485ff1614f9d3d935a953d99b811
leg7 <dev@leonardgomez.xyz>
2024-11-16 14:55:39 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "env for trollog";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+
+  outputs = { nixpkgs, ... }:
+  let system = "x86_64-linux";
+  in
+  {
+    devShells."${system}".default =
+    let pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell
+    {
+      packages = with pkgs; [
+        ghc
+      ];
+    };
+  };
+}
