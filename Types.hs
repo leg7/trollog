c5b912562f4e269055e775755f44e0369921bc68
leg7 <dev@leonardgomez.xyz>
2024-11-19 00:27:13 +0100

    Types: human format print

--- Types.hs
+++ Types.hs
@@ -1,4 +1,5 @@
 module Types where
+import Data.List (intersperse)
 
 -- Predicate
 
@@ -6,9 +7,9 @@ data PredicateArgs = StringArg String | IntArg Int | PredicateArg Predicate
                    deriving (Eq)
 
 instance Show PredicateArgs where
-    show (IntArg i) = "IntArg " ++ show i
-    show (PredicateArg p) = "\nPredicateArg " ++ show p
-    show (StringArg a) = "StringArg " ++ show a
+    show (IntArg i) = show i
+    show (PredicateArg p) = show p
+    show (StringArg a) = a
 
 data Predicate = Predicate { predicateAlias :: Maybe String,
                              predicateNegated :: Bool,
@@ -18,21 +19,17 @@ data Predicate = Predicate { predicateAlias :: Maybe String,
 
 instance Show Predicate where
     show (Predicate alias negated name args) =
-        "Predicate {\n" ++
-        "  Alias: " ++ showAlias alias ++ ",\n" ++
-        "  Negated: " ++ show negated ++ ",\n" ++
-        "  Name: " ++ name ++ ",\n" ++
-        "  Args: [" ++ showArgs args ++ "]\n" ++
-        "}"
+      let showNeg = if negated then "\\" else ""
+          in showNeg ++ name ++ showAlias alias ++ "(" ++ myShowList args ++ ")"
 
 showAlias :: Maybe String -> String
-showAlias Nothing  = "None"
-showAlias (Just a) = a
+showAlias Nothing  = ""
+showAlias (Just a) = "/" ++ a
 
-showArgs :: [PredicateArgs] -> String
-showArgs [] = ""
-showArgs [arg] = "  " ++ show arg
-showArgs (arg:args) = "  " ++ show arg ++ ",\n" ++ showArgs args
+myShowList :: Show a => [a] -> String
+myShowList [] = ""
+myShowList [arg] = show arg
+myShowList (arg:args) = show arg ++ ", " ++ myShowList args
 
 instance Ord Predicate where
     compare p1 p2 = compare (predicateName p1) (predicateName p2)
@@ -52,8 +49,9 @@ data Type = Str | N | P [String]
 
 instance Show Type where
     show Str       = "Str"
-    show N         = "N"
-    show (P args)  = "P[" ++ unwords args ++ "]"
+    show N         = "Int"
+    show (P args)  = unwords $ intersperse "|" args
+
 
 data TypeDef = TypeDef { typeName :: String,
                          typeArgs :: [Type]
@@ -70,16 +68,7 @@ instance Ord TypeDef where
 
 instance Show TypeDef where
     show (TypeDef name args) =
-        "TypeDef {\n" ++
-        "  typeName: " ++ name ++ ",\n" ++
-        "  typeArgs: [" ++ showTypeArgs args ++ "]\n" ++
-        "}\n"
-
--- Helper function to format the type arguments
-showTypeArgs :: [Type] -> String
-showTypeArgs [] = ""
-showTypeArgs [t] = show t
-showTypeArgs (t:ts) = show t ++ ", " ++ showTypeArgs ts
+      name ++ "(" ++ myShowList args ++ ")"
 
 -- Rules
 
@@ -92,16 +81,7 @@ data Rule = Rule { premises :: Conjunction,
 -- Show instance for Rule with proper formatting
 instance Show Rule where
     show (Rule prem cons) =
-        "Rule {\n" ++
-        "  Premises: [" ++ showConjunction prem ++ "]\n" ++
-        "  Consequences: [" ++ showConjunction cons ++ "]\n" ++
-        "}"
-
--- Helper function to format the Conjunction (list of Predicate)
-showConjunction :: Conjunction -> String
-showConjunction [] = ""
-showConjunction [p] = "  " ++ show p
-showConjunction (p:ps) = "  " ++ show p ++ ",\n" ++ showConjunction ps
+        "Rule { " ++ myShowList prem ++ " -> " ++ myShowList cons ++ " }\n"
 
 -- Expr
 
